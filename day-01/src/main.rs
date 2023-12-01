febf0dc84a9be1e4bb0c4ef9f98d5be03326b1be
Ryan Parsley <RMParsley@gmail.com>
2023-11-30 22:04:27 -0500

    chore: stub out day 1

---
+++ day-01/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    process()
+}
+
+fn process() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
