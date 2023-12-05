5fa80f2025f20a1f5a6b98d258ae4cc7b526149c
Ryan Parsley <RMParsley@gmail.com>
2023-12-05 16:32:39 -0500

    feat: solve day4 part 1

--- day-04/src/bin/part1.rs
+++ day-04/src/bin/part1.rs
@@ -5,7 +5,29 @@ fn main() {
 }
 
 fn process(input: &str) -> i32 {
-    input.lines().map(|_| 1).sum()
+    input
+        .lines()
+        .map(|card| {
+            let card: Vec<&str> = card.split(":").collect::<Vec<&str>>()[1]
+                .split("|")
+                .collect::<Vec<&str>>();
+
+            let winners: Vec<&str> = card[0].split_whitespace().collect::<Vec<&str>>();
+            let ours: Vec<_> = card[1].split_whitespace().collect::<Vec<_>>();
+
+            let our_winners = ours
+                .iter()
+                .filter(|number| winners.contains(number))
+                .collect::<Vec<_>>();
+
+            if our_winners.len() == 0 {
+                return 0;
+            }
+
+            let base: i32 = 2;
+            base.pow((our_winners.len() as u32) - 1)
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -16,6 +38,10 @@ mod tests {
     fn it_works() {
         let input = include_str!("mock-1.txt");
         let result = 13;
+        assert_eq!(result, process(input));
+
+        let input = include_str!("../../input1.txt");
+        let result = 18619;
         assert_eq!(result, process(input));
     }
 }
