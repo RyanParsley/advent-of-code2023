49b1a19cba87038fb32343f6c29f7cfbb2479deb
Ryan Parsley <RMParsley@gmail.com>
2023-12-07 22:04:42 -0500

    feat: solve day 6 part 1

--- day-06/src/bin/part1.rs
+++ day-06/src/bin/part1.rs
@@ -4,8 +4,47 @@ fn main() {
     println!("The answer is: {answer}");
 }
 
+struct Race {
+    time: i32,
+    distance: i32,
+}
+
 fn process(input: &str) -> i32 {
-    input.lines().map(|_| 1).sum()
+    let times: Vec<i32> = input.lines().collect::<Vec<&str>>()[0]
+        .split_whitespace()
+        .collect::<Vec<&str>>()[1..]
+        .iter()
+        .map(|num| num.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let distances: Vec<i32> = input.lines().collect::<Vec<&str>>()[1]
+        .split_whitespace()
+        .collect::<Vec<&str>>()[1..]
+        .iter()
+        .map(|num| num.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let races = times
+        .iter()
+        .enumerate()
+        .map(|(index, time)| Race {
+            time: *time,
+            distance: distances[index],
+        })
+        .collect::<Vec<Race>>();
+
+    races
+        .iter()
+        .map(|race| {
+            let mut score = 0;
+            for i in 1..race.time {
+                if i * (race.time - i) > race.distance {
+                    score += 1
+                }
+            }
+            score
+        })
+        .fold(1, |acc, solutions| acc * solutions)
 }
 
 #[cfg(test)]
@@ -15,7 +54,11 @@ mod tests {
     #[test]
     fn it_works() {
         let input = include_str!("mock-1.txt");
-        let result = 42;
+        let result = 288;
+        assert_eq!(result, process(input));
+
+        let input = include_str!("../../input1.txt");
+        let result = 2612736;
         assert_eq!(result, process(input));
     }
 }
