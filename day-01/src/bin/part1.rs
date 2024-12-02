0216914cc9bfbb0a8043d817448134957f3a1101
Ryan Parsley <RMParsley@gmail.com>
2024-12-02 18:31:53 -0500

    feat: solve day 1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     let input = include_str!("../../input1.txt");
     let output = process(input);
@@ -6,7 +5,47 @@ fn main() {
 }
 
 fn process(input: &str) -> i32 {
-    input
+    let numbers = input
+        .lines()
+        .map(parse_numbers_from_string)
+        .collect::<Vec<_>>();
+
+    let mut left = numbers
+        .clone()
+        .into_iter()
+        .map(|line| line.unwrap().0)
+        .collect::<Vec<_>>();
+
+    let mut right = numbers
+        .clone()
+        .into_iter()
+        .map(|line| line.unwrap().1)
+        .collect::<Vec<_>>();
+
+    left.sort();
+    right.sort();
+
+    elementwise_subtraction(right, left).into_iter().sum()
+}
+
+fn parse_numbers_from_string(input: &str) -> Option<(i32, i32)> {
+    let parts: Vec<&str> = input.split_whitespace().collect();
+    if parts.len() != 2 {
+        return None; // Not exactly two numbers separated by whitespace
+    }
+
+    let num1 = parts[0].parse::<i32>().ok()?;
+    let num2 = parts[1].parse::<i32>().ok()?;
+
+    Some((num1, num2))
+}
+
+fn elementwise_subtraction(vec_a: Vec<i32>, vec_b: Vec<i32>) -> Vec<i32> {
+    vec_a
+        .into_iter()
+        .zip(vec_b)
+        .map(|(a, b)| (a - b).abs())
+        .collect()
 }
 
 #[cfg(test)]
@@ -16,7 +55,7 @@ mod tests {
     #[test]
     fn it_works() {
         let input = include_str!("mock-1.txt");
-        let result = 42;
+        let result = 11;
         assert_eq!(result, process(input));
     }
 }
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,12 +1,45 @@
 
 fn main() {
-    let input = include_str!("../../input2.txt");
+    let input = include_str!("../../input1.txt");
     let output = process(input);
     println!("The answer is: {output}");
 }
 
 fn process(input: &str) -> i32 {
-    input
+    let numbers = input
+        .lines()
+        .map(parse_numbers_from_string)
+        .collect::<Vec<_>>();
+
+    let left = numbers
+        .clone()
+        .into_iter()
+        .map(|line| line.unwrap().0)
+        .collect::<Vec<_>>();
+
+    let right = numbers
+        .clone()
+        .into_iter()
+        .map(|line| line.unwrap().1)
+        .collect::<Vec<_>>();
+
+    left.into_iter().map(|value| value * count_occurances(value, right.clone())).sum()
+}
+
+fn count_occurances(num: i32, collection: Vec<i32>) -> i32 {
+    collection.iter().filter(|&n| *n == num).count() as  i32
+}
+
+fn parse_numbers_from_string(input: &str) -> Option<(i32, i32)> {
+    let parts: Vec<&str> = input.split_whitespace().collect();
+    if parts.len() != 2 {
+        return None; // Not exactly two numbers separated by whitespace
+    }
+
+    let num1 = parts[0].parse::<i32>().ok()?;
+    let num2 = parts[1].parse::<i32>().ok()?;
+
+    Some((num1, num2))
 }
 
 #[cfg(test)]
@@ -16,7 +49,7 @@ mod tests {
     #[test]
     fn it_works() {
         let input = include_str!("mock-1.txt");
-        let result = 42;
+        let result = 31;
         assert_eq!(result, process(input));
     }
 }
