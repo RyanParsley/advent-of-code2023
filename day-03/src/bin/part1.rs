f2ff05a22215fe065eb9dc491c9ced5d57db0881
Ryan Parsley <RMParsley@gmail.com>
2024-12-03 08:28:15 -0500

    chore: start day 3

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -5,7 +5,25 @@ fn main() {
 }
 
 fn process(input: &str) -> i32 {
+    parse_string_numbers(input)
+        .into_iter()
+        .map(|(a, b)| a * b)
+        .sum()
+}
+
+fn parse_string_numbers(input: &str) -> Vec<(i32, i32)> {
     input
+        .split("mul(")
+        .map(|chunk| chunk.split(")").collect::<Vec<_>>()[0])
+        .map(|chunk| chunk.split(",").collect::<Vec<_>>())
+        .filter(|chunk| chunk.len() > 1)
+        .map(|chunk| {
+            (
+                chunk[0].parse::<i32>().unwrap_or(0),
+                chunk[1].parse::<i32>().unwrap_or(0),
+            )
+        })
+        .collect::<Vec<_>>()
 }
 
 #[cfg(test)]
@@ -15,7 +33,8 @@ mod tests {
     #[test]
     fn it_works() {
         let input = include_str!("mock-1.txt");
-        let result = 42;
+        let result = 161;
         assert_eq!(result, process(input));
+        // too high 160683556
     }
 }
