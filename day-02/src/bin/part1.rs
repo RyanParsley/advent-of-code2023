0270cd63dc73d6e5e84d68303d011df2ea316249
Ryan Parsley <RMParsley@gmail.com>
2024-12-02 23:35:40 -0500

    feat: solve day 2 part 1

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -6,7 +6,22 @@ fn main() {
 }
 
 fn process(input: &str) -> i32 {
-    input
+    input.lines().map(parse_line_to_nums).map(validate_numbers).sum()
+}
+
+fn parse_line_to_nums(line: &str) -> Vec<i32> {
+    line.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
+
+fn validate_numbers(nums: Vec<i32>) -> i32 {
+    let isAscending = nums[0] < nums[1]; 
+    for i in 0..nums.len()-1 {
+       if (nums[i] - nums[i+1]).abs() > 3 { return 0; }
+       if nums[i] == nums[i+1] { return 0; }
+            if isAscending && nums[i]>nums[i+1] { return 0;}
+            if !isAscending && nums[i]<nums[i+1] { return 0;}
+    }
+    1
 }
 
 #[cfg(test)]
@@ -16,7 +31,24 @@ mod tests {
     #[test]
     fn it_works() {
         let input = include_str!("mock-1.txt");
-        let result = 42;
+        let result = 2;
         assert_eq!(result, process(input));
+    }
+
+    #[test]
+    fn it_parses_lines_to_numbers () {
+        let result = vec![1,2,3,4,5];
+        assert_eq!(result, parse_line_to_nums(&"1 2 3 4 5"));
+    }
+    #[test]
+    fn it_validates_numbers () {
+        assert_eq!(0, validate_numbers(vec![1,1,1,1,1]));
+        assert_eq!(1, validate_numbers(vec![1,2,3,4,5]));
+        assert_eq!(1, validate_numbers(vec![7,6,4,2,1]));
+        assert_eq!(0, validate_numbers(vec![1,2,7,8,9]));
+        assert_eq!(0, validate_numbers(vec![9,7,6,2,1]));
+        assert_eq!(0, validate_numbers(vec![1,3,2,4,5]));
+        assert_eq!(0, validate_numbers(vec![8,6,4,4,1]));
+        assert_eq!(1, validate_numbers(vec![1,3,6,7,9]));
     }
 }
--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -1,12 +1,53 @@
-
 fn main() {
-    let input = include_str!("../../input2.txt");
+    let input = include_str!("../../input1.txt");
     let output = process(input);
     println!("The answer is: {output}");
 }
 
 fn process(input: &str) -> i32 {
     input
+        .lines()
+        .map(parse_line_to_nums)
+        .map(validate_numbers)
+        .sum()
+}
+
+fn parse_line_to_nums(line: &str) -> Vec<i32> {
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn validate_numbers(mut nums: Vec<i32>) -> i32 {
+    let mut first_fault = false;
+    let mut i = 0;
+    let is_ascending = nums[0] < nums[1];
+
+    loop {
+        if i >= nums.len() - 1 {
+            break;
+        }
+        if compare_by_index(nums.clone(), i, i + 1, is_ascending) {
+            if first_fault {
+                return 0;
+            }
+            first_fault = true;
+            nums.remove(i + 1);
+            if nums.len() > i + 1 && compare_by_index(nums.clone(), i, i + 1, is_ascending) {
+                return 0;
+            }
+            continue;
+        }
+        i += 1;
+    }
+    1
+}
+
+fn compare_by_index(collection: Vec<i32>, a: usize, b: usize, is_ascending: bool) -> bool {
+    (collection[a] - collection[b]).abs() > 3
+        || collection[a] == collection[b]
+        || is_ascending && collection[a] > collection[b]
+        || !is_ascending && collection[a] < collection[b]
 }
 
 #[cfg(test)]
@@ -16,8 +57,26 @@ mod tests {
     #[test]
     fn it_works() {
         let input = include_str!("mock-1.txt");
-        let result = 42;
+        let result = 4;
         assert_eq!(result, process(input));
     }
-}
 
+    #[test]
+    fn it_parses_lines_to_numbers() {
+        let result = vec![1, 2, 3, 4, 5];
+        assert_eq!(result, parse_line_to_nums(&"1 2 3 4 5"));
+    }
+    #[test]
+    fn it_validates_numbers() {
+        assert_eq!(0, validate_numbers(vec![1, 1, 1, 1, 1]));
+        assert_eq!(1, validate_numbers(vec![1, 2, 3, 4, 5]));
+        assert_eq!(1, validate_numbers(vec![7, 6, 4, 2, 1]));
+        assert_eq!(1, validate_numbers(vec![8, 6, 4, 4, 1]));
+        assert_eq!(1, validate_numbers(vec![1, 3, 6, 7, 9]));
+        assert_eq!(1, validate_numbers(vec![1, 3, 2, 4, 5]));
+        assert_eq!(0, validate_numbers(vec![1, 2, 7, 8, 9]));
+        assert_eq!(0, validate_numbers(vec![9, 7, 6, 2, 1]));
+        // 449 is too low
+        // 626
+    }
+}
