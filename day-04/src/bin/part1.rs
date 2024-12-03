1e2609770fd43c8bcfc79c08244a6acbaece300e
Ryan Parsley <RMParsley@gmail.com>
2024-12-03 08:27:50 -0500

    chore: stub out day 04

---
+++ day-04/src/bin/part1.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let input = include_str!("../../input1.txt");
+    let output = process(input);
+    println!("The answer is: {output}");
+}
+
+fn process(input: &str) -> i32 {
+    input
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = include_str!("mock-1.txt");
+        let result = 42;
+        assert_eq!(result, process(input));
+    }
+}
