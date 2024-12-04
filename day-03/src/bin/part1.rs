52348f659bae96528d04ef207f5fe147a02f2e6f
Ryan Parsley <RMParsley@gmail.com>
2024-12-03 21:15:16 -0500

    feat: solve day 3 part 1

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -1,5 +1,6 @@
 fn main() {
     let input = include_str!("../../input1.txt");
+    //let input = include_str!("mock-1.txt");
     let output = process(input);
     println!("The answer is: {output}");
 }
@@ -12,18 +13,19 @@ fn process(input: &str) -> i32 {
 }
 
 fn parse_string_numbers(input: &str) -> Vec<(i32, i32)> {
-    input
+    let result = input
         .split("mul(")
         .map(|chunk| chunk.split(")").collect::<Vec<_>>()[0])
         .map(|chunk| chunk.split(",").collect::<Vec<_>>())
-        .filter(|chunk| chunk.len() > 1)
+        .filter(|chunk| chunk.len() == 2)
         .map(|chunk| {
             (
                 chunk[0].parse::<i32>().unwrap_or(0),
                 chunk[1].parse::<i32>().unwrap_or(0),
             )
         })
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>();
+    result
 }
 
 #[cfg(test)]
@@ -35,6 +37,14 @@ mod tests {
         let input = include_str!("mock-1.txt");
         let result = 161;
         assert_eq!(result, process(input));
+        // too high 160683556
+    }
+
+    #[test]
+    fn it_parses_a_bad_string_to_nothing() {
+        let input = "what()mul(758,157(][&+how()";
+        let result = vec![(758, 0)];
+        assert_eq!(result, parse_string_numbers(input));
         // too high 160683556
     }
 }
